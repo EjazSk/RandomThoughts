eba2828fabe62ba2a2108a67178445c957b66f64
Ejaz <ejaz.sk124@gmail.com>
2017-02-05 19:27:24 +0530

    Rivision of static

--- randomthoughts/settings.py
+++ randomthoughts/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*-(n*ptie)z5xx)ii%0^ni6hwz3hz=j5%b(0&3idwloc-wbsd@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['rndtht.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
