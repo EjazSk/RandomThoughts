c408d20fbb1835f908a89cd186ba4561487c8cdc
Ejaz <ejaz.sk124@gmail.com>
2017-02-05 19:17:32 +0530

    Rivision of static

--- randomthoughts/wsgi.py
+++ randomthoughts/wsgi.py
@@ -11,12 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "randomthoughts.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "randomthoughts.settings")
 
 
 
