61d8db88f94f3307d21e49138641c2cb30b03e16
Ejaz <ejaz.sk124@gmail.com>
2017-02-05 15:48:22 +0530

    Adding Bootstrap iles

--- randomthoughts/settings.py
+++ randomthoughts/settings.py
@@ -129,14 +129,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_dip","static_root")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_dip","static_root")
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static_in_prod","our_statics"),
 )
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
